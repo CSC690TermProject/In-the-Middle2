3ef27d4cbf232fc3654babaed99fa0f87502c76c
Justin Abarquez <justincabarquez@gmail.com>
2018-04-15 11:48:07 -0700

    Button and location textfields are now at desired locations. Will now begin to work on functionality.

--- InTheMiddle2/InTheMiddle2/ViewController.swift
+++ InTheMiddle2/InTheMiddle2/ViewController.swift
@@ -14,29 +14,48 @@ class ViewController: UIViewController, UISearchBarDelegate{
     @IBOutlet weak var googleMapsContainer: UIView!
     
     var googleMapsView: GMSMapView!
+    var minimalStyle: GMSMapStyle!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         //map properties
-        let camera = GMSCameraPosition.camera(withLatitude: 37.7749, longitude: -122.478, zoom: 10.25)
+        let camera = GMSCameraPosition.camera(withLatitude: 37.74, longitude: -122.478, zoom: 10.25)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
+        minimalStyle = try! GMSMapStyle.init(contentsOfFileURL: Bundle.main.url(forResource: "modernminimal", withExtension: "json")!)
+        mapView.mapStyle = minimalStyle
         
-        //DINE button
-        let dine = UIButton(frame: CGRect(x: 250, y: 550, width: 100, height: 50))
-        dine.backgroundColor = .black
+        //DINE button properties
+        let dine = UIButton(frame: CGRect(x: 235, y: 550, width: 150, height: 75))
+        dine.backgroundColor = .gray
         dine.setTitle("DINE", for: .normal)
+        dine.titleLabel?.font =  UIFont(name: "AvenirNextCondensed-DemiBold", size: 50)
+        dine.titleLabel?.textColor = UIColor(red: 204/255, green: 255/255, blue: 204/255, alpha: 1.0)
         dine.addTarget(self, action:#selector(self.dinePressed), for: .touchUpInside)
         self.view.addSubview(dine)
 
+        //location field properties
+        let locationA =  UITextField(frame: CGRect(x: 20, y: 425, width: 350, height: 40))
+        locationA.placeholder = "A"
+        locationA.font = UIFont(name: "Avenir-Book", size: 25)
+        locationA.borderStyle = UITextBorderStyle.roundedRect
+        self.view.addSubview(locationA)
+        
+        let locationB =  UITextField(frame: CGRect(x: 20, y: 485, width: 350, height: 40))
+        locationB.placeholder = "B"
+        locationB.font = UIFont(name: "Avenir-Book", size: 25)
+        locationB.borderStyle = UITextBorderStyle.roundedRect
+        self.view.addSubview(locationB)
+        
         //bar properties
         navigationItem.title = "DINE in the MIDDLE"
         self.navigationController?.navigationBar.barStyle  = UIBarStyle.black
-        self.navigationController?.navigationBar.barTintColor   = UIColor(red: 153/255, green: 204/255, blue: 0/255, alpha: 1.0)
+        self.navigationController?.navigationBar.barTintColor   = .gray //UIColor(red: 153/255, green: 204/255, blue: 0/255, alpha: 1.0)
         self.navigationController?.view.backgroundColor = .clear
         self.navigationController?.navigationBar.isTranslucent = true
-        let navigationTitleFont = UIFont(name: "Avenir", size: 22)!
+        let navigationTitleFont = UIFont(name: "AvenirNextCondensed-DemiBoldItalic",  size: 27)!
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.font: navigationTitleFont]
     }
     
@@ -44,12 +63,9 @@ class ViewController: UIViewController, UISearchBarDelegate{
         print("yay u did it")
     }
 
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
