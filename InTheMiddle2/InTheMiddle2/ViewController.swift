7ade3d18dab1d7a4a7885b011e99b7c040114207
Justin Abarquez <justinchristopher@Justins-MacBook-Air.local>
2018-04-19 17:35:57 -0700

    Setting up Location Manager delegates.

--- InTheMiddle2/InTheMiddle2/ViewController.swift
+++ InTheMiddle2/InTheMiddle2/ViewController.swift
@@ -9,16 +9,33 @@
 import UIKit
 import GoogleMaps
 
-class ViewController: UIViewController, UISearchBarDelegate{
+enum Location {
+    case locationA
+    case locationB
+}
+
+class ViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var googleMapsContainer: UIView!
     
+    var locationManager = CLLocationManager()
+    var locationSelected = Location.locationA
+    var locationStart = CLLocation()
+    var locationEnd = CLLocation()
+    
     var googleMapsView: GMSMapView!
     var minimalStyle: GMSMapStyle!
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startMonitoringSignificantLocationChanges()
 
         //map properties
         let camera = GMSCameraPosition.camera(withLatitude: 37.74, longitude: -122.478, zoom: 10.25)
@@ -27,27 +44,33 @@ class ViewController: UIViewController, UISearchBarDelegate{
         minimalStyle = try! GMSMapStyle.init(contentsOfFileURL: Bundle.main.url(forResource: "modernminimal", withExtension: "json")!)
         mapView.mapStyle = minimalStyle
         
+        self.googleMapsView?.camera = camera
+        self.googleMapsView?.delegate = self
+        self.googleMapsView?.isMyLocationEnabled = true
+        self.googleMapsView?.settings.myLocationButton = true
+        self.googleMapsView?.settings.zoomGestures = true
+        
         //DINE button properties
-        let dine = UIButton(frame: CGRect(x: 235, y: 550, width: 150, height: 75))
-        dine.backgroundColor = .gray
-        dine.setTitle("DINE", for: .normal)
-        dine.titleLabel?.font =  UIFont(name: "AvenirNextCondensed-DemiBold", size: 50)
-        dine.titleLabel?.textColor = UIColor(red: 204/255, green: 255/255, blue: 204/255, alpha: 1.0)
-        dine.addTarget(self, action:#selector(self.dinePressed), for: .touchUpInside)
-        self.view.addSubview(dine)
+        let dineButton = UIButton(frame: CGRect(x: 250, y: 600, width: 112.5, height: 56.25))
+        dineButton.backgroundColor = .gray
+        dineButton.setTitle("DINE", for: .normal)
+        dineButton.titleLabel?.font =  UIFont(name: "AvenirNextCondensed-DemiBold", size: 40)
+        dineButton.titleLabel?.textColor = UIColor(red: 204/255, green: 255/255, blue: 204/255, alpha: 1.0)
+        dineButton.addTarget(self, action:#selector(self.dinePressed), for: .touchUpInside)
+        self.view.addSubview(dineButton)
 
         //location field properties
-        let locationA =  UITextField(frame: CGRect(x: 20, y: 425, width: 350, height: 40))
-        locationA.placeholder = "A"
-        locationA.font = UIFont(name: "Avenir-Book", size: 25)
-        locationA.borderStyle = UITextBorderStyle.roundedRect
-        self.view.addSubview(locationA)
+        let locationATF =  UITextField(frame: CGRect(x: 20, y: 490, width: 300, height: 40))
+        locationATF.placeholder = "A"
+        locationATF.font = UIFont(name: "Avenir-Book", size: 25)
+        locationATF.borderStyle = UITextBorderStyle.roundedRect
+        self.view.addSubview(locationATF)
         
-        let locationB =  UITextField(frame: CGRect(x: 20, y: 485, width: 350, height: 40))
-        locationB.placeholder = "B"
-        locationB.font = UIFont(name: "Avenir-Book", size: 25)
-        locationB.borderStyle = UITextBorderStyle.roundedRect
-        self.view.addSubview(locationB)
+        let locationBTF =  UITextField(frame: CGRect(x: 20, y: 545, width: 300, height: 40))
+        locationBTF.placeholder = "B"
+        locationBTF.font = UIFont(name: "Avenir-Book", size: 25)
+        locationBTF.borderStyle = UITextBorderStyle.roundedRect
+        self.view.addSubview(locationBTF)
         
         //bar properties
         navigationItem.title = "DINE in the MIDDLE"
@@ -58,6 +81,24 @@ class ViewController: UIViewController, UISearchBarDelegate{
         let navigationTitleFont = UIFont(name: "AvenirNextCondensed-DemiBoldItalic",  size: 27)!
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.font: navigationTitleFont]
     }
+    
+    //creates a marker pin on the map
+    func createMarker(titleMarker: String, iconMarker: UIImage, latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        let marker =  GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(latitude, longitude)
+        marker.title = titleMarker
+        marker.icon = iconMarker
+        marker.map = googleMapsView
+    }
+    
+    //Location Manager delegates
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error){
+        print("Error to get location: \(error)")
+    }
+    
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        let location = locations.last
+//    }
     
     @objc func dinePressed() {
         print("yay u did it")
---
+++ InTheMiddle2/Pods/Target Support Files/Pods-InTheMiddle2/Pods-InTheMiddle2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InTheMiddle2 : NSObject
+@end
+@implementation PodsDummy_Pods_InTheMiddle2
+@end
